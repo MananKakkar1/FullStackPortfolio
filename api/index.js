6521b8a378dc5cb9fd75270b0ab3e71dd346b452
kakkarm2 <manan.kakkar@mail.utoronto.ca>
2025-05-22 16:41:01 -0400

    Added error checking

--- api/index.js
+++ api/index.js
@@ -25,5 +25,6 @@ export default async function handler(req, res) {
     res.status(200).json({ success: true });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
+    console.error("Error sending email:", error);
   }
 }
